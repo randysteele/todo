9e8dfc9229bb938f850cd271ba482b0a4661aeb8
steelerx155 <steelerx155@gmail.com>
2021-03-19 11:40:20 -0400

    updated index and create user actions

--- .history/app/controllers/users_controller_20210319112928.rb
+++ .history/app/controllers/users_controller_20210319112928.rb
@@ -1,25 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
-  def index 
-    @user = User.new #creates a user object for our form
-    end
-
-    def create
-      @user = User.create(signup_params) 
-      session[user_id] = user.id
-      if @user.save 
-       {
-        redirect_to example.url
-       } 
-      else 
-       {
-        render :index
-       }
-      end
-   end
-    
-
+  # GET /users or /users.json
+  def index
+    @users = User.all
+  end
 
   # GET /users/1 or /users/1.json
   def show
@@ -35,6 +20,19 @@ class UsersController < ApplicationController
   end
 
   # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
